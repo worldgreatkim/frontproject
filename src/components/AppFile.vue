b72677ec4029a61c7586f547fa7374b4ace443b3
worldgreatkim <edwin.dkim@gmail.com>
2024-11-25 11:15:56 +0900

    1

--- src/components/AppFile.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <v-file-input label="File input w/ chips" chips multiple></v-file-input>
-    <v-file-input label="File input w/ small chips" multiple small-chips></v-file-input>
-  </div>
-</template>
-<script></script>
-<style scoped></style>
